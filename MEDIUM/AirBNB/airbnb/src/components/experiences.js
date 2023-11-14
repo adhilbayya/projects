0407fef0281e0681831718d4859074aadbbbe568
AdhilBayya <adhilvk445@gmail.com>
2023-11-14 22:04:21 +0530

    Added cards using props

--- MEDIUM/AirBNB/airbnb/src/components/experiences.js
+++ MEDIUM/AirBNB/airbnb/src/components/experiences.js
@@ -2,21 +2,19 @@ import Star from "./logos/Star.png";
 export default function ExperienceSection(props) {
   return (
     <div className="experience-section">
-      <div className="swimmer-section">
-        <img src={`../img/${props.img}`} className="picture-section"></img>
+      <img src={`../img/${props.img}`} className="picture-section"></img>
 
-        <p className="card-rating">
-          <img src={Star} className="star-pic"></img>
-          {props.currentRating}
-          <span style={{ color: "#918E9B" }}>
-            &nbsp;({props.totalRating}) • {props.country}
-          </span>
-        </p>
-        <p className="card-title">{props.cardTitle}</p>
-        <p className="rate">
-          <span className="bold">From ${props.rate}</span> / person
-        </p>
-      </div>
+      <p className="card-rating">
+        <img src={Star} className="star-pic"></img>
+        {props.currentRating}
+        <span style={{ color: "#918E9B" }}>
+          &nbsp;({props.totalRating}) • {props.country}
+        </span>
+      </p>
+      <p className="card-title">{props.cardTitle}</p>
+      <p className="rate">
+        <span className="bold">From ${props.rate}</span> / person
+      </p>
     </div>
   );
 }
---
+++ MEDIUM/AirBNB/airbnb/src/components/mainExperiences.js
@@ -0,0 +1,18 @@
+import ExperienceSection from "./experiences";
+import data from "./data";
+
+export default function Experiences() {
+  let experienceElement = data.map((d) => {
+    return (
+      <ExperienceSection
+        img={d.coverImg}
+        currentRating={d.stats.rating}
+        totalRating={d.stats.reviewCount}
+        country={d.location}
+        cardTitle={d.title}
+        rate={d.price}
+      />
+    );
+  });
+  return <div className="card-section">{experienceElement}</div>;
+}
