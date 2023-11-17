ba6c6e8468d51a69f86a7c5e30ab65f639050b22
AdhilBayya <adhilvk445@gmail.com>
2023-11-17 14:59:40 +0530

    Travel journal is done

--- MEDIUM/travel-journal/src/components/data.js
+++ MEDIUM/travel-journal/src/components/data.js
@@ -14,9 +14,9 @@ export default [
     id: 2,
     title: "Thalasherry",
     location: "Kerala, India",
-    googleMapLink: "https://maps.app.goo.gl/Y9WTK8ro9kDoAJRP7",
-    startDate: "Sep 18 2023",
-    endDate: "Sep 21 2023",
+    googleMapLink: "https://maps.app.goo.gl/KkUuU6q1rWqyPQNK6",
+    startDate: "Mar 18 2023",
+    endDate: "Mar 20 2023",
     description:
       "Thalassery, also known as Tellicherry, is a city in the Kannur District of Kerala, India. It is the second largest city in North Malabar.Thalassery is a coastal town on the Malabar Coast of the Arabian Sea. It was established in 1683 by the British for the pepper and cardamom trade. Thalassery is one of the oldest British settlements in India. Thalassery is known as the political and cultural capital of Malabar. It was the center of learning in north Kerala during the colonial era",
     imgURL: "IMG_2477.JPG",
