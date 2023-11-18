879b970bad0c9f5139a5c0fb7448dbcec28d6534
AdhilBayya <adhilvk445@gmail.com>
2023-11-18 18:42:21 +0530

    Added some changes

--- MEDIUM/meme-generator/src/components/input.js
+++ MEDIUM/meme-generator/src/components/input.js
@@ -1,24 +1,27 @@
 import memesData from "./memesData";
+import React from "react";
 
 export default function InputArea() {
-  function generateMeme() {
+  let [memeImage, setMemeImg] = React.useState("");
+  function GenerateMeme() {
     let meme = memesData.data.memes;
     let randomMeme = Math.floor(Math.random() * meme.length);
     let url = meme[randomMeme].url;
-    console.log(url);
+    setMemeImg(url);
   }
   return (
     <main>
       <div className="input-area">
-        <input type="text" className="first-input" value="Top line"></input>
-        <input className="second-input" type="text" value="Bottom line"></input>
+        <input type="text" className="form-input" value="Top line"></input>
+        <input className="sform-input" type="text" value="Bottom line"></input>
         <input
           type="button"
           className="button"
           value="Get a new meme image 🖼️"
-          onClick={generateMeme}
+          onClick={GenerateMeme}
         ></input>
       </div>
+      <img src={memeImage} className="meme-img"></img>
     </main>
   );
 }
